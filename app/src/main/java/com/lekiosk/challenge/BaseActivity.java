41122718cc19f9565e1f4e09a3145b254fa3291b
BadrAtt <badr.elattaoui@gmail.com>
2019-06-01 01:02:58 +0000

    add home ui

--- app/src/main/java/com/lekiosk/challenge/BaseActivity.java
+++ app/src/main/java/com/lekiosk/challenge/BaseActivity.java
@@ -3,11 +3,29 @@ package com.lekiosk.challenge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BaseActivity extends AppCompatActivity {
+import com.lekiosk.challenge.ui.home.HomeFragment;
+
+public class BaseActivity extends AppCompatActivity implements HomeFragment.OnHomeFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        initView();
+    }
+
+    //init our view with the HomeFragment
+    public void initView(){
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.base_container, HomeFragment.newInstance())
+                .commit();
+    }
+
+
+    @Override
+    public void onHomeFragmentInteraction() {
+
     }
 }
---
+++ app/src/main/java/com/lekiosk/challenge/ui/home/HomeFragment.java
@@ -0,0 +1,60 @@
+package com.lekiosk.challenge.ui.home;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.lekiosk.challenge.R;
+
+
+public class HomeFragment extends Fragment {
+
+    private OnHomeFragmentInteractionListener mListener;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnHomeFragmentInteractionListener) {
+            mListener = (OnHomeFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnHomeFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnHomeFragmentInteractionListener {
+        void onHomeFragmentInteraction();
+    }
+}
